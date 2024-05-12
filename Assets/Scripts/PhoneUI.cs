a9fc68224b040c9912d16f2e4c4b48ce670fc7be
Sabified <github@gogutten.no>
2024-05-12 12:08:41 +0200

    add final ui, add phone script
    Now there is art for the kitchen, Phone UI, and the time on the phone updates correctly to the user locally.

---
+++ Assets/Scripts/PhoneUI.cs
@@ -0,0 +1,49 @@
+using TMPro;
+using UnityEngine;
+
+public class PhoneUI : MonoBehaviour
+{
+    [System.Serializable]
+    class TextMessage
+    {
+        public GameObject parentObj;
+        public TMP_Text textObj;
+        [TextArea(2, 3)] public string message;
+
+        public void SetTextObject()
+        { textObj = parentObj.GetComponentInChildren<TMP_Text>(); }
+        public void SetMessageText()
+        { textObj.text = message; }
+        public void SetTextObjectAndText()
+        { SetTextObject(); SetMessageText(); }
+    }
+
+    [SerializeField] TextMessage message1;
+    [SerializeField] TextMessage message3;
+
+    bool reachedFirstCriteriaSpent = false;
+
+    [SerializeField] TMP_Text timeText;
+    [SerializeField] [TextArea(2, 5)] string reachedFirstCriteriaMessage =
+        "Credit Card Company:\nThere has been some suspicios activity on your account";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        message1.SetTextObjectAndText();
+        message3.SetTextObjectAndText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeText.text = System.DateTime.Now.ToString("HH:mm");
+
+        if (AudioManager.instance.firstCriteriaReached && !reachedFirstCriteriaSpent)
+        {
+            reachedFirstCriteriaSpent = true;
+            message3.message = reachedFirstCriteriaMessage;
+            message3.SetMessageText();
+        }
+    }
+}
